340b2ee7a8b446504fc47da6ce43b6b3c6d7465a
Yanni Fraga <yanni_fernanda@hotmail.com>
2018-09-13 22:51:08 -0300

    Pig game

--- pig-game/js/app.js
+++ pig-game/js/app.js
@@ -13,12 +13,33 @@ const diceUI = document.querySelector('.dice');
 const btnHold = document.querySelector('.btn-hold');
 const btnRoll = document.querySelector('.btn-roll');
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
+init();
 
-diceUI.classList.add('hidden');
-btnHold.classList.add('hidden');
+function init() {
+  scores = [0, 0];
+  activePlayer = 0;
+  roundScore = 0;
+
+  diceUI.classList.add('hidden');
+  btnHold.classList.add('hidden');
+  btnRoll.classList.remove('hidden');
+
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+  document.getElementById('name-0').textContent = 'Player 1';
+  document.getElementById('name-1').textContent = 'Player 2';
+
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+  document.querySelector('.player-0-panel').classList.remove('active');
+  document.querySelector('.player-1-panel').classList.remove('active');
+  document.querySelector('.player-0-panel').classList.add('active');
+}
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
 
 btnRoll.addEventListener('click', function() {
   
@@ -75,3 +96,4 @@ function nextPlayer() {
   
   diceUI.classList.add('hidden');
 }
+
