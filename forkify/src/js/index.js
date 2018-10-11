d3b166665d9fa7d27589543c5e20247149605b97
Yanni Fraga <yanni_fernanda@hotmail.com>
2018-10-10 22:35:42 -0300

    Search pagination

--- forkify/src/js/index.js
+++ forkify/src/js/index.js
@@ -1,4 +1,6 @@
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements, renderLoader, clearLoader } from "./views/base";
 
 /*
   Global state of the app
@@ -11,23 +13,50 @@ const state = {};
 
 const controlSearch = async () => {
   // get query from view
-  const query = 'pizza';
-
+  const query = searchView.getInput();
+  
   if(query) {
     // new search object and add to state
     state.search = new Search(query);
 
     // prepare UI for results
+    searchView.clearInput();
+    searchView.clearResults();
+    renderLoader(elements.searchResults);
 
     // search for recipes
     await state.search.getResults();
 
     // render results on UI
-    console.log(state.search.result)
+    clearLoader();
+    searchView.renderResults(state.search.result);
   }
 };
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
   e.preventDefault();
   controlSearch();
 });
+
+elements.searchResultPages.addEventListener('click', e => {
+  let btn;
+
+  btn = e.target.classList.contains('.btn-inline') ? btn = e.target : e.target.closest('.btn-inline');
+  
+  if(btn) {
+    const goToPage = parseInt(btn.dataset.goto, 10);
+    searchView.clearResults();
+    searchView.renderResults(state.search.result, goToPage);
+  }
+});
+
+
+// Hi!
+
+// So that's how I did it:
+
+//  btn = e.target.classList.contains('.btn-inline') ? e.target : e.target.closest('.btn-inline'); 
+
+// If it's Firefox, then e.target will have the class, otherwise use e.target.closest
+
+// :)
