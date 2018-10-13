bd5b3476996f137042b857850b4d7747bd4525cc
Yanni Fraga <yanni_fernanda@hotmail.com>
2018-10-12 21:46:42 -0700

    List controller

--- forkify/src/js/index.js
+++ forkify/src/js/index.js
@@ -1,7 +1,9 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
+import * as listView from './views/listView';
 import { elements, renderLoader, clearLoader } from "./views/base";
 
 /*
@@ -69,6 +71,10 @@ const controlRecipe = async () => {
     recipeView.clearRecipe();
     renderLoader(elements.recipe);
 
+    // highlight selected search item
+    if(state.search)
+      searchView.highlightSelected(id);
+
     // create new recipe object
     state.recipe = new Recipe(id);
 
@@ -91,7 +97,57 @@ const controlRecipe = async () => {
   }
 };
 
+// LIST CONTROLLER
+const controlList = () => {
+  // create a new list if there is none yet
+  if(!state.list) 
+    state.list = new List();
+
+  // add each ingredient to the list and UI
+  state.recipe.ingredients.forEach(el => {
+    const item = state.list.addItem(el.count, el.unit, el.ingredient);
+    listView.renderItem(item);
+  });
+}
+
+//handle delete and update list item events
+elements.shopping.addEventListener('click', e => {
+  const id = e.target.closest('.shopping__item').dataset.itemid;
+  
+  if(e.target.matches('.shopping__delete, .shopping__delete *')) {
+    // delete from state
+    state.list.deleteItem(id);
+    // delete form UI
+    listView.deleteItem(id);
+  }
+  else if(e.target.matches('.shopping__count-value')) {
+    const val = parseFloat(e.target.value, 10);
+    state.list.updateCount(id, val);
+  }
+});
+
 // window.addEventListener('hashchange', controlRecipe);
 // window.addEventListener('load', controlRecipe);
 
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+
+// handling recipe button clicks
+elements.recipe.addEventListener('click', e => {
+  if(e.target.matches('.btn-decrease, .btn-decrease *')) {
+    // decrease button is clicked
+
+    if(state.recipe.servings > 1) {
+      state.recipe.updateServings('dec');
+      recipeView.updateServingsIngredients(state.recipe);
+    }
+  }
+  else if(e.target.matches('.btn-increase, .btn-increase *')) {
+    // increase button is clicked
+    state.recipe.updateServings('inc');
+    recipeView.updateServingsIngredients(state.recipe);
+  }
+  else if(e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+    controlList();
+  }
+});
+
--- forkify/src/js/views/searchView.js
+++ forkify/src/js/views/searchView.js
@@ -9,6 +9,16 @@ export const clearResults = () => {
   elements.searchResultPages.innerHTML = '';
 };
 
+export const highlightSelected = id => {
+  const resultsArr = Array.from(document.querySelectorAll('.results__link'));
+  
+  resultsArr.forEach(el => {
+    el.classList.remove('results__link--active');
+  });
+  
+  document.querySelector(`.results__link[href*="${id}"]`).classList.add('results__link--active');
+};
+
 const limitRecipeTitle = (title, limit = 17) => {
   const newTitle = [];
 
